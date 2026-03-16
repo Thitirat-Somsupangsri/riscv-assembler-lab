62993e1e74207ee7569eeab00b9c31039988d035
Thitirat <thitirat.som@ku.th>
2026-03-16 16:41:12 +0700

    add

--- examples/test_case_1.s
+++ examples/test_case_1.s
@@ -1,57 +1,106 @@
 start:
 addi x1, x0, 5
-addi x2, x0, 10
-addi x30, x0, 256
-add x3, x1, x2
-sub x4, x3, x1
-and x5, x3, x4
-or x6, x4, x2
-xor x7, x5, x6
-sll x8, x1, x0
-srl x9, x2, x0
-sra x10, x4, x0
-slt x11, x1, x2
-sltu x12, x1, x2
-addi x13, x0, 1
-andi x14, x13, 1
-ori x15, x14, 2
-xori x16, x15, 3
-slli x17, x16, 1
-srli x18, x17, 1
-srai x19, x18, 1
-slti x20, x1, 6
-sltiu x21, x1, 6
-sw x3, 0(x30)
-sh x4, 4(x30)
-sb x5, 8(x30)
-lw x22, 0(x30)
-lh x23, 4(x30)
-lb x24, 8(x30)
-lhu x25, 4(x30)
-lbu x26, 8(x30)
-beq x1, x2, skip_beq
-bne x1, x2, take_bne
-skip_beq:
-add x29, x0, x0
-take_bne:
-blt x1, x2, take_blt
-add x29, x29, x0
-take_blt:
-bge x2, x1, take_bge
-add x29, x29, x0
-take_bge:
-bltu x1, x2, take_bltu
-add x29, x29, x0
-take_bltu:
-bgeu x2, x1, take_bgeu
-add x29, x29, x0
-take_bgeu:
-jal x27, helper1
+add x30, x2, x0
+addi x2, x0, 12
+addi x3, x0, 2
+block1:
+add x4, x1, x2
+sub x5, x4, x3
+and x6, x4, x5
+or x7, x6, x2
+xor x8, x7, x1
+sll x9, x3, x1
+srl x10, x2, x3
+sra x11, x5, x3
+slt x12, x1, x2
+sltu x13, x1, x2
+addi x14, x14, 1
+andi x15, x14, 7
+ori x16, x15, 4
+xori x17, x16, 3
+slli x18, x17, 1
+srli x19, x18, 1
+srai x20, x19, 1
+slti x21, x1, 8
+sltiu x22, x1, 8
+sw x4, 0(x30)
+sh x5, 8(x30)
+sb x6, 16(x30)
+lw x23, 0(x30)
+lh x24, 8(x30)
+lb x25, 16(x30)
+lhu x26, 8(x30)
+lbu x27, 16(x30)
+beq x1, x2, block1_beq
+bne x1, x2, block1_bne
+block1_beq:
+add x28, x0, x0
+block1_bne:
+blt x1, x2, block1_blt
+add x28, x28, x0
+block1_blt:
+bge x2, x1, block1_bge
+add x28, x28, x0
+block1_bge:
+bltu x1, x2, block1_bltu
+add x28, x28, x0
+block1_bltu:
+bgeu x2, x1, block1_bgeu
+add x28, x28, x0
+block1_bgeu:
+addi x1, x1, 1
+block2:
+add x4, x1, x2
+sub x5, x4, x3
+and x6, x4, x5
+or x7, x6, x2
+xor x8, x7, x1
+sll x9, x3, x1
+srl x10, x2, x3
+sra x11, x5, x3
+slt x12, x1, x2
+sltu x13, x1, x2
+addi x14, x14, 1
+andi x15, x14, 7
+ori x16, x15, 4
+xori x17, x16, 3
+slli x18, x17, 1
+srli x19, x18, 1
+srai x20, x19, 1
+slti x21, x1, 8
+sltiu x22, x1, 8
+sw x4, 4(x30)
+sh x5, 12(x30)
+sb x6, 20(x30)
+lw x23, 4(x30)
+lh x24, 12(x30)
+lb x25, 20(x30)
+lhu x26, 12(x30)
+lbu x27, 20(x30)
+beq x1, x2, block2_beq
+bne x1, x2, block2_bne
+block2_beq:
+add x28, x0, x0
+block2_bne:
+blt x1, x2, block2_blt
+add x28, x28, x0
+block2_blt:
+bge x2, x1, block2_bge
+add x28, x28, x0
+block2_bge:
+bltu x1, x2, block2_bltu
+add x28, x28, x0
+block2_bltu:
+bgeu x2, x1, block2_bgeu
+add x28, x28, x0
+block2_bgeu:
+jal x29, helper1
 after_helper1:
-add x29, x22, x23
+add x5, x23, x24
+addi x10, x0, 10
 beq x0, x0, end1
 helper1:
-add x28, x24, x25
-jalr x0, x27, 0
+add x6, x25, x26
+jalr x0, x29, 0
 end1:
-beq x0, x0, end1
+ecall
--- examples/test_case_3.s
+++ examples/test_case_3.s
@@ -1,13 +1,13 @@
-entry:
+start:
 addi x1, x0, 1
+add x30, x2, x0
 addi x2, x0, 64
 addi x3, x0, 2
-addi x30, x0, 512
-section1:
+block1:
 add x4, x1, x2
-sub x5, x2, x1
+sub x5, x4, x3
 and x6, x4, x5
-or x7, x6, x3
+or x7, x6, x2
 xor x8, x7, x1
 sll x9, x3, x1
 srl x10, x2, x3
@@ -24,36 +24,36 @@ srai x20, x19, 1
 slti x21, x1, 40
 sltiu x22, x1, 40
 sw x4, 0(x30)
-sh x5, 32(x30)
-sb x6, 64(x30)
+sh x5, 24(x30)
+sb x6, 48(x30)
 lw x23, 0(x30)
-lh x24, 32(x30)
-lb x25, 64(x30)
-lhu x26, 32(x30)
-lbu x27, 64(x30)
-beq x1, x2, section1_beq
-bne x1, x2, section1_bne
-section1_beq:
+lh x24, 24(x30)
+lb x25, 48(x30)
+lhu x26, 24(x30)
+lbu x27, 48(x30)
+beq x1, x2, block1_beq
+bne x1, x2, block1_bne
+block1_beq:
 add x28, x0, x0
-section1_bne:
-blt x1, x2, section1_blt
+block1_bne:
+blt x1, x2, block1_blt
 add x28, x28, x0
-section1_blt:
-bge x2, x1, section1_bge
+block1_blt:
+bge x2, x1, block1_bge
 add x28, x28, x0
-section1_bge:
-bltu x1, x2, section1_bltu
+block1_bge:
+bltu x1, x2, block1_bltu
 add x28, x28, x0
-section1_bltu:
-bgeu x2, x1, section1_bgeu
+block1_bltu:
+bgeu x2, x1, block1_bgeu
 add x28, x28, x0
-section1_bgeu:
+block1_bgeu:
 addi x1, x1, 1
-section2:
+block2:
 add x4, x1, x2
-sub x5, x2, x1
+sub x5, x4, x3
 and x6, x4, x5
-or x7, x6, x3
+or x7, x6, x2
 xor x8, x7, x1
 sll x9, x3, x1
 srl x10, x2, x3
@@ -70,36 +70,36 @@ srai x20, x19, 1
 slti x21, x1, 40
 sltiu x22, x1, 40
 sw x4, 4(x30)
-sh x5, 36(x30)
-sb x6, 68(x30)
+sh x5, 28(x30)
+sb x6, 52(x30)
 lw x23, 4(x30)
-lh x24, 36(x30)
-lb x25, 68(x30)
-lhu x26, 36(x30)
-lbu x27, 68(x30)
-beq x1, x2, section2_beq
-bne x1, x2, section2_bne
-section2_beq:
+lh x24, 28(x30)
+lb x25, 52(x30)
+lhu x26, 28(x30)
+lbu x27, 52(x30)
+beq x1, x2, block2_beq
+bne x1, x2, block2_bne
+block2_beq:
 add x28, x0, x0
-section2_bne:
-blt x1, x2, section2_blt
+block2_bne:
+blt x1, x2, block2_blt
 add x28, x28, x0
-section2_blt:
-bge x2, x1, section2_bge
+block2_blt:
+bge x2, x1, block2_bge
 add x28, x28, x0
-section2_bge:
-bltu x1, x2, section2_bltu
+block2_bge:
+bltu x1, x2, block2_bltu
 add x28, x28, x0
-section2_bltu:
-bgeu x2, x1, section2_bgeu
+block2_bltu:
+bgeu x2, x1, block2_bgeu
 add x28, x28, x0
-section2_bgeu:
+block2_bgeu:
 addi x1, x1, 1
-section3:
+block3:
 add x4, x1, x2
-sub x5, x2, x1
+sub x5, x4, x3
 and x6, x4, x5
-or x7, x6, x3
+or x7, x6, x2
 xor x8, x7, x1
 sll x9, x3, x1
 srl x10, x2, x3
@@ -116,36 +116,36 @@ srai x20, x19, 1
 slti x21, x1, 40
 sltiu x22, x1, 40
 sw x4, 8(x30)
-sh x5, 40(x30)
-sb x6, 72(x30)
+sh x5, 32(x30)
+sb x6, 56(x30)
 lw x23, 8(x30)
-lh x24, 40(x30)
-lb x25, 72(x30)
-lhu x26, 40(x30)
-lbu x27, 72(x30)
-beq x1, x2, section3_beq
-bne x1, x2, section3_bne
-section3_beq:
+lh x24, 32(x30)
+lb x25, 56(x30)
+lhu x26, 32(x30)
+lbu x27, 56(x30)
+beq x1, x2, block3_beq
+bne x1, x2, block3_bne
+block3_beq:
 add x28, x0, x0
-section3_bne:
-blt x1, x2, section3_blt
+block3_bne:
+blt x1, x2, block3_blt
 add x28, x28, x0
-section3_blt:
-bge x2, x1, section3_bge
+block3_blt:
+bge x2, x1, block3_bge
 add x28, x28, x0
-section3_bge:
-bltu x1, x2, section3_bltu
+block3_bge:
+bltu x1, x2, block3_bltu
 add x28, x28, x0
-section3_bltu:
-bgeu x2, x1, section3_bgeu
+block3_bltu:
+bgeu x2, x1, block3_bgeu
 add x28, x28, x0
-section3_bgeu:
+block3_bgeu:
 addi x1, x1, 1
-section4:
+block4:
 add x4, x1, x2
-sub x5, x2, x1
+sub x5, x4, x3
 and x6, x4, x5
-or x7, x6, x3
+or x7, x6, x2
 xor x8, x7, x1
 sll x9, x3, x1
 srl x10, x2, x3
@@ -162,36 +162,36 @@ srai x20, x19, 1
 slti x21, x1, 40
 sltiu x22, x1, 40
 sw x4, 12(x30)
-sh x5, 44(x30)
-sb x6, 76(x30)
+sh x5, 36(x30)
+sb x6, 60(x30)
 lw x23, 12(x30)
-lh x24, 44(x30)
-lb x25, 76(x30)
-lhu x26, 44(x30)
-lbu x27, 76(x30)
-beq x1, x2, section4_beq
-bne x1, x2, section4_bne
-section4_beq:
+lh x24, 36(x30)
+lb x25, 60(x30)
+lhu x26, 36(x30)
+lbu x27, 60(x30)
+beq x1, x2, block4_beq
+bne x1, x2, block4_bne
+block4_beq:
 add x28, x0, x0
-section4_bne:
-blt x1, x2, section4_blt
+block4_bne:
+blt x1, x2, block4_blt
 add x28, x28, x0
-section4_blt:
-bge x2, x1, section4_bge
+block4_blt:
+bge x2, x1, block4_bge
 add x28, x28, x0
-section4_bge:
-bltu x1, x2, section4_bltu
+block4_bge:
+bltu x1, x2, block4_bltu
 add x28, x28, x0
-section4_bltu:
-bgeu x2, x1, section4_bgeu
+block4_bltu:
+bgeu x2, x1, block4_bgeu
 add x28, x28, x0
-section4_bgeu:
+block4_bgeu:
 addi x1, x1, 1
-section5:
+block5:
 add x4, x1, x2
-sub x5, x2, x1
+sub x5, x4, x3
 and x6, x4, x5
-or x7, x6, x3
+or x7, x6, x2
 xor x8, x7, x1
 sll x9, x3, x1
 srl x10, x2, x3
@@ -208,36 +208,36 @@ srai x20, x19, 1
 slti x21, x1, 40
 sltiu x22, x1, 40
 sw x4, 16(x30)
-sh x5, 48(x30)
-sb x6, 80(x30)
+sh x5, 40(x30)
+sb x6, 64(x30)
 lw x23, 16(x30)
-lh x24, 48(x30)
-lb x25, 80(x30)
-lhu x26, 48(x30)
-lbu x27, 80(x30)
-beq x1, x2, section5_beq
-bne x1, x2, section5_bne
-section5_beq:
+lh x24, 40(x30)
+lb x25, 64(x30)
+lhu x26, 40(x30)
+lbu x27, 64(x30)
+beq x1, x2, block5_beq
+bne x1, x2, block5_bne
+block5_beq:
 add x28, x0, x0
-section5_bne:
-blt x1, x2, section5_blt
+block5_bne:
+blt x1, x2, block5_blt
 add x28, x28, x0
-section5_blt:
-bge x2, x1, section5_bge
+block5_blt:
+bge x2, x1, block5_bge
 add x28, x28, x0
-section5_bge:
-bltu x1, x2, section5_bltu
+block5_bge:
+bltu x1, x2, block5_bltu
 add x28, x28, x0
-section5_bltu:
-bgeu x2, x1, section5_bgeu
+block5_bltu:
+bgeu x2, x1, block5_bgeu
 add x28, x28, x0
-section5_bgeu:
+block5_bgeu:
 addi x1, x1, 1
-section6:
+block6:
 add x4, x1, x2
-sub x5, x2, x1
+sub x5, x4, x3
 and x6, x4, x5
-or x7, x6, x3
+or x7, x6, x2
 xor x8, x7, x1
 sll x9, x3, x1
 srl x10, x2, x3
@@ -254,130 +254,37 @@ srai x20, x19, 1
 slti x21, x1, 40
 sltiu x22, x1, 40
 sw x4, 20(x30)
-sh x5, 52(x30)
-sb x6, 84(x30)
-lw x23, 20(x30)
-lh x24, 52(x30)
-lb x25, 84(x30)
-lhu x26, 52(x30)
-lbu x27, 84(x30)
-beq x1, x2, section6_beq
-bne x1, x2, section6_bne
-section6_beq:
-add x28, x0, x0
-section6_bne:
-blt x1, x2, section6_blt
-add x28, x28, x0
-section6_blt:
-bge x2, x1, section6_bge
-add x28, x28, x0
-section6_bge:
-bltu x1, x2, section6_bltu
-add x28, x28, x0
-section6_bltu:
-bgeu x2, x1, section6_bgeu
-add x28, x28, x0
-section6_bgeu:
-addi x1, x1, 1
-section7:
-add x4, x1, x2
-sub x5, x2, x1
-and x6, x4, x5
-or x7, x6, x3
-xor x8, x7, x1
-sll x9, x3, x1
-srl x10, x2, x3
-sra x11, x5, x3
-slt x12, x1, x2
-sltu x13, x1, x2
-addi x14, x14, 2
-andi x15, x14, 31
-ori x16, x15, 4
-xori x17, x16, 9
-slli x18, x17, 1
-srli x19, x18, 1
-srai x20, x19, 1
-slti x21, x1, 40
-sltiu x22, x1, 40
-sw x4, 0(x30)
-sh x5, 32(x30)
-sb x6, 64(x30)
-lw x23, 0(x30)
-lh x24, 32(x30)
-lb x25, 64(x30)
-lhu x26, 32(x30)
-lbu x27, 64(x30)
-beq x1, x2, section7_beq
-bne x1, x2, section7_bne
-section7_beq:
-add x28, x0, x0
-section7_bne:
-blt x1, x2, section7_blt
-add x28, x28, x0
-section7_blt:
-bge x2, x1, section7_bge
-add x28, x28, x0
-section7_bge:
-bltu x1, x2, section7_bltu
-add x28, x28, x0
-section7_bltu:
-bgeu x2, x1, section7_bgeu
-add x28, x28, x0
-section7_bgeu:
-addi x1, x1, 1
-section8:
-add x4, x1, x2
-sub x5, x2, x1
-and x6, x4, x5
-or x7, x6, x3
-xor x8, x7, x1
-sll x9, x3, x1
-srl x10, x2, x3
-sra x11, x5, x3
-slt x12, x1, x2
-sltu x13, x1, x2
-addi x14, x14, 2
-andi x15, x14, 31
-ori x16, x15, 4
-xori x17, x16, 9
-slli x18, x17, 1
-srli x19, x18, 1
-srai x20, x19, 1
-slti x21, x1, 40
-sltiu x22, x1, 40
-sw x4, 4(x30)
-sh x5, 36(x30)
+sh x5, 44(x30)
 sb x6, 68(x30)
-lw x23, 4(x30)
-lh x24, 36(x30)
+lw x23, 20(x30)
+lh x24, 44(x30)
 lb x25, 68(x30)
-lhu x26, 36(x30)
+lhu x26, 44(x30)
 lbu x27, 68(x30)
-beq x1, x2, section8_beq
-bne x1, x2, section8_bne
-section8_beq:
+beq x1, x2, block6_beq
+bne x1, x2, block6_bne
+block6_beq:
 add x28, x0, x0
-section8_bne:
-blt x1, x2, section8_blt
+block6_bne:
+blt x1, x2, block6_blt
 add x28, x28, x0
-section8_blt:
-bge x2, x1, section8_bge
+block6_blt:
+bge x2, x1, block6_bge
 add x28, x28, x0
-section8_bge:
-bltu x1, x2, section8_bltu
+block6_bge:
+bltu x1, x2, block6_bltu
 add x28, x28, x0
-section8_bltu:
-bgeu x2, x1, section8_bgeu
+block6_bltu:
+bgeu x2, x1, block6_bgeu
 add x28, x28, x0
-section8_bgeu:
-addi x1, x1, 1
+block6_bgeu:
 jal x29, helper3
 after_helper3:
 add x5, x23, x24
-add x6, x25, x26
+addi x10, x0, 10
 beq x0, x0, end3
 helper3:
-add x7, x27, x22
+add x6, x25, x26
 jalr x0, x29, 0
 end3:
-beq x0, x0, end3
+ecall
